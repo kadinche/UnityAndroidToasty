58708acb8b55502761df563dca1e47ce47234b89
KOGA Mitsuhiro <mitsuhiro.koga@kadinche.com>
2020-06-29 14:17:51 +0900

    Add success button

---
+++ UnityAndroidToasty/Assets/Scripts/Toasty.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kadinche.Example.Plugin
+{
+    public class Toasty : MonoBehaviour
+    {
+        [SerializeField] private Text countText = default;
+
+        private AndroidJavaClass _jClass;
+        private AndroidJavaObject _jInstance;
+        private AndroidPluginCallback _callback;
+
+        private void OnEnable()
+        {
+            _jClass = new AndroidJavaClass("com.kadinche.example.toastywrapper.ToastyFragment");
+            _jInstance = _jClass.CallStatic<AndroidJavaObject>("getInstance");
+            _callback = new AndroidPluginCallback(countText);
+        }
+
+        private void OnDisable()
+        {
+            _jInstance?.Dispose();
+            _jClass?.Dispose();
+        }
+
+        public void Success(string message)
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            _jInstance.Call("success", message, _callback);
+#endif
+        }
+    }
+
+    public class AndroidPluginCallback : AndroidJavaProxy
+    {
+        private readonly Text _text;
+
+        public AndroidPluginCallback(Text text) : base("com.kadinche.example.toastywrapper.PluginCallback")
+        {
+            _text = text;
+        }
+
+        public void sendResponse(int count)
+        {
+            _text.text = $"{count}";
+        }
+    }
+}
